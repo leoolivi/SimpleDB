44b8ad5804fa50b7bf3ce9ec34dc470af7b76898
leoolivi <leolivieri1910@gmail.com>
2026-04-15 23:39:47 +0200

    feat: add specialized factory implementations
    - Implement CLICommandFactory for creating CLI commands
    - Implement QueryCommandFactory for creating query commands

---
+++ src/main/java/it/leo/main/factories/QueryCommandFactory.java
@@ -0,0 +1,30 @@
+package it.leo.main.factories;
+
+import java.util.List;
+
+import it.leo.main.QueryCommand;
+
+public class QueryCommandFactory {
+
+    public static final QueryCommand GET = QueryCommand.builder().setName("GET")
+        .setHasArg(true)
+        .setDescription("Get a value from key. Example: GET key")
+        .build();
+
+    public static final QueryCommand GETALL = QueryCommand.builder().setName("GETALL")
+        .setHasArg(false)
+        .setDescription("Get all values. Example: GETALL")
+        .build();
+
+    public static final QueryCommand SET = QueryCommand.builder().setName("SET")
+        .setHasArg(true)
+        .setDescription("Set a key with value. Example: SET key TO value")
+        .setSubCommands(List.of(QueryCommand.builder()
+            .setName("TO")
+            .setHasArg(true)
+            .setDescription("Indicates the value")
+            .build()))
+        .build();
+
+    public static final List<QueryCommand> ALL = List.of(GET, SET, GETALL);
+}
